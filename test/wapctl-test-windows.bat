b21ab8346e141c54a1526861e31389e811a36486
Miki Yutani <mkyutani@gmail.com>
2020-10-18 15:32:08 +0900

    #13

---
+++ test/wapctl-test-windows.bat
@@ -0,0 +1,47 @@
+python wapctl --help
+
+python wapctl pkg start calc -y
+python wapctl p
+python wapctl p list wap-name-which-does-not-exist
+python wapctl p list calc
+python wapctl p show calc -s,
+python wapctl p stop calculator -y
+
+python wapctl a
+python wapctl a list wap-name-which-does-not-exist
+python wapctl a list “d‘ì
+python wapctl a show “d‘ì -s,
+python wapctl a start “d‘ì -y
+python wapctl p stop calculator -y
+
+python wapctl ins
+python wapctl ins list wap-name-which-does-not-exist
+python wapctl ins list zoom
+python wapctl ins show zoom -s,
+python wapctl ins remove zoom
+
+python wapctl pkg
+python wapctl pkg list wap-name-which-does-not-exist
+python wapctl pkg list paint
+python wapctl pkg show paint -s,
+python wapctl pkg start microsoft.mspaint -y
+python wapctl p stop paintstudio -y
+python wapctl pkg remove paint
+
+python wapctl svc
+python wapctl svc list wap-name-which-does-not-exist
+python wapctl svc list docker
+python wapctl svc show docker -s,
+python wapctl svc stop docker -y
+python wapctl svc show docker -s,
+python wapctl svc start docker -y
+python wapctl svc show docker -s,
+
+python wapctl run
+python wapctl run list wap-name-which-does-not-exist
+python wapctl run list docker
+python wapctl run show docker -s,
+python wapctl run stop docker -y
+python wapctl run show docker -s,
+python wapctl run start docker -y
+python wapctl run show docker -s,

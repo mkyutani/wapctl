7cfb23d6fab9d0badd5dfac3bc240cd0b593e9be
Miki Yutani <mkyutani@gmail.com>
2020-10-25 10:54:34 +0900

    #15

--- test/wapctl-test-windows.bat
+++ test/wapctl-test-windows.bat
@@ -4,26 +4,26 @@ python wapctl pkg start calc -y
 python wapctl p
 python wapctl p list wap-name-which-does-not-exist
 python wapctl p list calc
-python wapctl p show calc -s,
+python wapctl p show calc -s, -H
 python wapctl p stop calculator -y
 
 python wapctl a
 python wapctl a list wap-name-which-does-not-exist
 python wapctl a list 電卓
-python wapctl a show 電卓 -s,
+python wapctl a show 電卓 -s, -H
 python wapctl a start 電卓 -y
 python wapctl p stop calculator -y
 
 python wapctl ins
 python wapctl ins list wap-name-which-does-not-exist
 python wapctl ins list zoom
-python wapctl ins show zoom -s,
+python wapctl ins show zoom -s, -H
 python wapctl ins remove zoom
 
 python wapctl pkg
 python wapctl pkg list wap-name-which-does-not-exist
 python wapctl pkg list paint
-python wapctl pkg show paint -s,
+python wapctl pkg show paint -s, -H
 python wapctl pkg start microsoft.mspaint -y
 python wapctl p stop paintstudio -y
 python wapctl pkg remove paint
@@ -31,17 +31,17 @@ python wapctl pkg remove paint
 python wapctl svc
 python wapctl svc list wap-name-which-does-not-exist
 python wapctl svc list docker
-python wapctl svc show docker -s,
+python wapctl svc show docker -s, -H
 python wapctl svc stop docker -y
-python wapctl svc show docker -s,
+python wapctl svc show docker -s, -H
 python wapctl svc start docker -y
-python wapctl svc show docker -s,
+python wapctl svc show docker -s, -H
 
 python wapctl run
 python wapctl run list wap-name-which-does-not-exist
 python wapctl run list docker
-python wapctl run show docker -s,
+python wapctl run show docker -s, -H
 python wapctl run stop docker -y
-python wapctl run show docker -s,
+python wapctl run show docker -s, -H
 python wapctl run start docker -y
-python wapctl run show docker -s,
+python wapctl run show docker -s, -H
